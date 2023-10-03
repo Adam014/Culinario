dbec7e444b11ba8545efe0cb2ec000e3b6df75fd
Adam Stádník <adam.stadnik@seznam.cz>
2023-10-03 21:06:56 +0200

    Added the style for sidebar

--- culinario/src/components/main.tsx
+++ culinario/src/components/main.tsx
@@ -4,7 +4,7 @@ const main = () => {
   return (
     <main className="main">
       <h1>Welcome back $username</h1>
-      <h2>What are you <h3>cooking</h3> today?</h2>
+      <h2>What are you cooking today?</h2>
     </main>
   )
 }
--- culinario/src/components/sidebar.tsx
+++ culinario/src/components/sidebar.tsx
@@ -1,25 +1,33 @@
-import { Recipes, Home, Heart } from "../images/images"
+import { useState } from "react"
+import { Recipes, Home, Heart } from "../images/images";
 
 const sidebar = () => {
-  return (
-    <aside className="aside">
-        <div className="aside-container">
-            <h4>Discover</h4>
-            <div className="home-container">
-                <img src={Home} alt="home-icon"/>
-                <h6>Home</h6>
+
+    const [activeTab, setActiveTab] = useState(null);
+
+    const handleClick = (tab) => {
+        setActiveTab(tab);
+    };
+
+    return (
+        <aside className="aside">
+            <div className="aside-container">
+                <h4>Discover</h4>
+                <div className={`${activeTab === "home" ? "home-container-active" : "home-container"}`} onClick={() => handleClick("home")}>
+                    <img src={Home} alt="home-icon"/>
+                    <h6>Home</h6>
+                </div>
+                <div className={`${activeTab === "favorite" ? "favorite-container-active" : "favorite-container"}`} onClick={() => handleClick("favorite")}>
+                    <img src={Heart} alt="favorite-icon"/>
+                    <h6>Favorites</h6>
+                </div>
+                <div className={`${activeTab === "recipes" ? "recipes-container-active" : "recipes-container"}`} onClick={() => handleClick("recipes")}>
+                    <img src={Recipes} alt="recipes-icon"/>
+                    <h6>All recipes</h6>
+                </div>
             </div>
-            <div className="favorite-container">
-                <img src={Heart} alt="favorite-icon"/>
-                <h6>Favorites</h6>
-            </div>
-            <div className="recipes-container">
-                <img src={Recipes} alt="recipes-icon"/>
-                <h6>All recipes</h6>
-            </div>
-        </div>
-    </aside>
-  )
+        </aside>
+    )
 }
 
 export default sidebar
