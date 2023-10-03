42e1436db082382f051f64645af9790136c65e05
Adam Stádník <adam.stadnik@seznam.cz>
2023-10-03 21:56:32 +0200

    Added profile component

---
+++ culinario/src/components/profile.tsx
@@ -0,0 +1,14 @@
+
+// pridat profile component
+// po kliknuti zobrazit misto mainBlog, profiůe
+// zobrazit accessToken, email, isVerified, isAnon, Created, LastLogged atd atd
+
+const profile = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default profile
