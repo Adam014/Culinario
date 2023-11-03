d1f67937e6f91ba3a5c133cf252e80c07e53f99f
Adam014 <adam.stadnik@seznam.cz>
2023-11-03 22:09:26 +0100

    Re-moved the favorite button in single recipe card, styled it more

--- culinario/src/components/services/allRecipes.tsx
+++ culinario/src/components/services/allRecipes.tsx
@@ -26,12 +26,6 @@ const AllRecipes: React.FC<AllRecipesProps> = ({ simplified }: AllRecipesProps) 
 
   // state for the searchTerm in searchBar
   const [searchTerm, setSearchTerm] = useState("");
-  
-  // state for the array of favorites, loading from localStorage
-  const [favorites, setFavorites] = useState<Recipe[]>(() => {
-    const storedFavorites = localStorage.getItem("favorites");
-    return storedFavorites ? JSON.parse(storedFavorites) : [];
-  });
 
   // Memoize the filtered data to avoid unnecessary re-renders
   const filteredData = useMemo(() => {
@@ -42,25 +36,6 @@ const AllRecipes: React.FC<AllRecipesProps> = ({ simplified }: AllRecipesProps) 
     }
     return [];
   }, [recipesList, searchTerm]);
-
-  // function for toggling the recipes into Favorite mode
-  const toggleFavorites = (recipe: Recipe) => {
-    // Create a timestamp
-    const timestamp = new Date().getTime();
-    const updatedRecipe = { ...recipe, favoritedAt: timestamp }; // Add favoritedAt property
-
-    if (favorites.some((favRecipe) => favRecipe.id === recipe.id)) {
-      // Recipe is already in favorites, so remove it
-      const updatedFavorites = favorites.filter((favRecipe) => favRecipe.id !== recipe.id);
-      setFavorites(updatedFavorites);
-      localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
-    } else {
-      // Recipe is not in favorites, so add it with the timestamp
-      const updatedFavorites = [...favorites, updatedRecipe];
-      setFavorites(updatedFavorites);
-      localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
-    }
-  };
 
   // checking if the API data is fetching
   if (isFetching) return "Loading...";
@@ -79,13 +54,6 @@ const AllRecipes: React.FC<AllRecipesProps> = ({ simplified }: AllRecipesProps) 
       <div className='recipe-card-container'>
         {filteredData.map((recipe: Recipe) => (
           <div className="recipe-card" key={recipe.id}>
-            {/* add a hearth icon instead of a classic button */}
-            <button onClick={() => toggleFavorites(recipe)} className='button-favorite'>
-              {favorites.some((favRecipe) => favRecipe.id === recipe.id)
-                ? <i className="fa-solid fa-heart remove-favorite"></i>
-                : <i className="fa-regular fa-heart add-favorite"></i>
-              }
-            </button>
             {/* custom component */}
             <RecipeCard key={recipe.id} recipe={recipe} />
           </div>
--- culinario/src/components/services/recipeCard.tsx
+++ culinario/src/components/services/recipeCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Recipe } from './allRecipes'; 
 import { ThumbsUp, Time } from '../../images/images'; 
 import { Link } from 'react-router-dom';
@@ -12,8 +12,39 @@ interface RecipeCardProps {
 // save the favorite to db instead of localStorage
 
 const RecipeCard: React.FC<RecipeCardProps> = ({ recipe }: RecipeCardProps) => {
+  // state for the array of favorites, loading from localStorage
+  const [favorites, setFavorites] = useState<Recipe[]>(() => {
+    const storedFavorites = localStorage.getItem("favorites");
+    return storedFavorites ? JSON.parse(storedFavorites) : [];
+  });
+
+  // function for toggling the recipes into Favorite mode
+  const toggleFavorites = (recipe: Recipe) => {
+    // Create a timestamp
+    const timestamp = new Date().getTime();
+    const updatedRecipe = { ...recipe, favoritedAt: timestamp }; // Add favoritedAt property
+
+    if (favorites.some((favRecipe) => favRecipe.id === recipe.id)) {
+      // Recipe is already in favorites, so remove it
+      const updatedFavorites = favorites.filter((favRecipe) => favRecipe.id !== recipe.id);
+      setFavorites(updatedFavorites);
+      localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
+    } else {
+      // Recipe is not in favorites, so add it with the timestamp
+      const updatedFavorites = [...favorites, updatedRecipe];
+      setFavorites(updatedFavorites);
+      localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
+    }
+  };
+
   return (
-    <div className="recipe-card">
+    <div className='recipe-card'>
+      <button onClick={() => toggleFavorites(recipe)} className='button-favorite'>
+          {favorites.some((favRecipe) => favRecipe.id === recipe.id)
+            ? <i className="fa-solid fa-heart remove-favorite"></i>
+            : <i className="fa-regular fa-heart add-favorite"></i>
+          }
+      </button>
       <Link to={`/recipe/${recipe.id}`} key={recipe.id}>
         <div className="recipe-info">
           <img className="recipe-image" src={recipe.thumbnail_url} alt={recipe.name} />
