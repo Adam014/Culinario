1e3d0ca3257cfd46c768cc2a127be1505bffed95
Adam Stádník <adam.stadnik@seznam.cz>
2023-11-10 15:31:26 +0100

    Commit

--- culinario/src/components/services/recipeDetails.tsx
+++ culinario/src/components/services/recipeDetails.tsx
@@ -1,21 +1,22 @@
 import React from "react";
 import Layout from '../Layout/Layout';
 import { User } from 'firebase/auth';
+import { Recipe } from "./allRecipes";
 import { useParams } from "react-router-dom";
 
 interface RecipeDetailsProps {
     authUser: User | null; 
     setAuthUser: React.Dispatch<React.SetStateAction<User | null>>;
+    recipe: Recipe;
   }
 
 const recipeDetails: React.FC<RecipeDetailsProps> = ({ authUser, setAuthUser }) => {
     const recipeId = useParams();
     
     console.log(recipeId);
-
     return (
         <Layout authUser={authUser} setAuthUser={setAuthUser}>
-            <div>Single Recipe</div>
+            <div></div>
         </Layout>
     )
 }
